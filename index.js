7e8dd431a102056bf972653b3c3d9ececc5108ab
Ruslanwest <ruslan.label2@gmail.com>
2024-01-30 16:45:24 +0700

    dari kami budaya barat

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Ruslan Permana" }');
+    res.send('{ "response": "Hello From Asep kebarat-baratan" }');
 });
 
 app.get('/home', function (req, res) {
-    res.send('{ "response": "Hello World Home" }');
+    res.send('{ "response": "Hello World Jalu Segala solusi" }');
 });
 app.get('/about', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works! Yuda" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
