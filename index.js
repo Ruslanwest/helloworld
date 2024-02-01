3ac4f1855c550e69ff20226f024e891ef3d3aac3
Ruslanwest <ruslan.label2@gmail.com>
2024-02-01 15:16:38 +0700

    BS-8 <message>

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/home', function (req, res) {
-    res.send('{ "response": "Hello World Jalu Segala solusi" }');
+    res.send('{ "response": "Hello World Jalu Segala solusi anda" }');
 });
 app.get('/about', function (req, res) {
     res.send('{ "response": " Great!, It works! Yuda" }');
